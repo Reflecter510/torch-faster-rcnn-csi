044d529a91740932dbe07d03554914ae17bdaeb5
reflecter510 <499300020@qq.com>
2021-04-24 23:57:43 +0800

    中期检查

--- model_test copy.py
+++ model_test copy.py
@@ -17,61 +17,55 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 import time
-
-
 from nets.unet_model import UNet
        
-os.system("rm input/detection-results/result*.txt")
-os.system("rm input/ground-truth/result*.txt")
-os.system("rm predict/*.png")
-
+# 数据集设置
 dataset_name = "192S1ALL"
 which_data = "test"
+
+'''模型断点路径'''
+# temporal  logs\\15-torch-TEMPORAL\\anchor4-bs36\\Epoch250-Total_Loss0.1010-Val_Loss0.2589.pth
+# s1        logs\\15-torch\\Epoch190-Total_Loss0.2849-Val_Loss0.3886.pth
+path_checkpoint = "logs\\15-torch\\Epoch190-Total_Loss0.2849-Val_Loss0.3886.pth" # 断点路径
+
+#结果可视化
+PLOT = True    
+SHOW = False
+
+# 主干特征提取网络
+BACKBONE = "alexnet"
+
+
+#--------------------------------------------------------------------------------------------
 IMAGE_SHAPE = utils_base.get_IMAGE_SHAPE_from_dataset_name(dataset_name)
 
 if dataset_name == "TEMPORAL":
     actions = ['none', "1", "2", "3", "4", "5", "6"]
     batch_size = 278
+    NUM_CLASSES = 6
+    N_CHANNELS = 52
+    ANCHOR = ((4*16,5*16,6*16,7*16,8*16,9*16,10*16),)
 else:
     actions = ['none', 'jump', 'pick', 'throw', 'pull', 'clap', 'box', 'wave', 'lift', 'kick', 'squat', 'turnRound', 'checkWatch']
     batch_size = 215
+    NUM_CLASSES = 12
+    N_CHANNELS = 90
+    ANCHOR = ((4*16,5*16,6*16,7*16,8*16,9*16,10*16),)
 
-'''
-加载测试集
-'''
+# 是否可以使用cuda
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+Cuda = torch.cuda.is_available()
+print(device)
+
+
+#加载测试集
 if dataset_name != "S":
     num_test_instances, test_data_loader = DataUtil.get_data_loader(dataset_name, which_data, batch_size=batch_size, shuffle = False)
 else:
     num_test_instances, test_data_loader = DataUtil.get_data_S_loader(which_data, batch_size=batch_size, shuffle = False)
 
 
-'''设置'''
-# temporal  logs\\15-torch-TEMPORAL\\anchor4-bs36\\Epoch250-Total_Loss0.1010-Val_Loss0.2589.pth
-# s1        logs\\15-torch\\Epoch190-Total_Loss0.2849-Val_Loss0.3886.pth
-path_checkpoint = "logs\\15-torch\\Epoch190-Total_Loss0.2849-Val_Loss0.3886.pth" # 断点路径
-nms_iou = 0.01
-score_thresh = 0.0
-PLOT = True    #结果可视化
-SHOW = False
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# Assuming that we are on a CUDA machine, this should print a CUDA device:
-print(device)
-
-Cuda = torch.cuda.is_available()
-
-#加载自定义的模型
-BACKBONE = "alexnet"
-if dataset_name == "TEMPORAL":
-    NUM_CLASSES = 6
-    N_CHANNELS = 52
-    ANCHOR = ((4*16,5*16,6*16,7*16,8*16,9*16,10*16),)#(4*16,5*16,6*16,7*16,8*16,9*16,10*16))#((2*16, 4*16,5*16,6*16,7*16,8*16,10*16),)
-else:
-    NUM_CLASSES = 12
-    N_CHANNELS = 90
-    ANCHOR = ((4*16,5*16,6*16,7*16,8*16,9*16,10*16),)#(4*16,5*16,6*16,7*16,8*16,9*16,10*16))
-
-
+#------------------------------  模型  ----------------------------------------#
 if BACKBONE == "alexnet":
     backbone = AlexNet(n_channels=N_CHANNELS, n_classes=NUM_CLASSES+1).features
 elif BACKBONE == "unet":
@@ -87,18 +81,28 @@ model = FasterRCNN(backbone,
                     num_classes=NUM_CLASSES+1,
                     rpn_anchor_generator=anchor_generator,
                     box_roi_pool=roi_pooler).to(device)
+#-----------------------------------------------------------------------------#
 
-checkpoint = torch.load(path_checkpoint,  map_location=device)  # 加载断点
-model.load_state_dict(checkpoint['net'])  # 加载模型可学习参数
 
+# 加载断点
+checkpoint = torch.load(path_checkpoint,  map_location=device)  
+# 加载模型可学习参数
+model.load_state_dict(checkpoint['net'])  
+
+
+#-----------------------------------------------------------------------------#
+# 测试模式
 model = model.eval()
+
+# 结果
 np.set_printoptions(suppress=True)
+
 ious_all = 0
 detection_all = 0.0
 acc = 0.0
+
 cnt = 0
 i = 0
-my_map = {}
 _time = 0.0
 
 for (data, bbox, label) in tqdm(test_data_loader):
@@ -113,20 +117,10 @@ for (data, bbox, label) in tqdm(test_data_loader):
             bboxV = Variable(bbox)
             labelV = Variable(label)
 
-        if False:
-            mkey = int(int((bbox[0][1]-bbox[0][0])/10)*10)
-            if not my_map.get(mkey, False):
-                my_map.update({mkey:0})
-            my_map[mkey] += 1
-        # if Cuda:
-        #     torch.cuda.synchronize()   #增加同步操作
         start = time.time()
-
         predictions = model(dataV.unsqueeze(3))
-
-        # if Cuda:
-        #     torch.cuda.synchronize() #增加同步操作
         end = time.time()
+
         _time += (end-start)
 
         for idp in range(0, len(predictions[1])):
@@ -142,7 +136,6 @@ for (data, bbox, label) in tqdm(test_data_loader):
 
             max_iou = bbox_iou(np.asarray(bbox[idx].view(1,2).cpu()), np.asarray(bboxV[idp].view(1,2)))[0][0]
             dete_acc = detection_acc(np.asarray(bbox[idx].view(1,2).cpu()), np.asarray(bboxV[idp].view(1,2)))[0][0]
-            
             
             if PLOT:
                 X=np.linspace(0,192,192,endpoint=True)
@@ -160,54 +153,22 @@ for (data, bbox, label) in tqdm(test_data_loader):
                 currentAxis.add_patch(rect)
                 
                 plt.xlabel(which_data+str(i)+"  prd = "+actions[int(label[idx])]+"  gt = "+actions[int(labelV[idp][0])]+"  acc = "+str(conf[idx].tolist())[:5]+"  iou = "+str(max_iou)[:5]+" wd = "+str(int(bboxV[idp][1]-bboxV[idp][0])))
-                if 1:
-                    # mkey = int(int((bboxV[idp][1]-bboxV[idp][0])/10)*10)
-                    # if not my_map.get(mkey, False):
-                    #     my_map.update({mkey:0})
-                    # my_map[mkey] += 1
-                    plt.savefig("predict/%d.png"%(i), dpi=128)
-                    #plt.show()
+                plt.savefig("predict/%d.png"%(i), dpi=128)
                 plt.close()
           
-            if SHOW:
-                fe = model.backbone
-                x = dataV.unsqueeze(3)
-                fe.eval()
-                for name, layer in fe._modules.items():
-                    x = layer(x)
-
-                    #if f'{name}'!='0' and f'{name}'!='3' and f'{name}'!= '5':
-                    #   continue
-
-                    X=np.linspace(0,192,192,endpoint=True)
-                    plt.contourf(x[0].squeeze().permute(1,0).cpu())
-                    plt.colorbar()
-                    currentAxis=plt.gca()
-                    plt.xlabel(which_data+str(i)+"  prd = "+actions[int(label[idx])]+"  gt = "+actions[int(labelV[0][0])]+"  acc = "+str(conf[idx].tolist())[:5]+"  iou = "+str(max_iou)[:5]+" wd = "+str(int(bboxV[0][1]-bboxV[0][0])))
-                    plt.savefig("feature_map_show/%d_layer_%s.png"%(i, f'{name}'), dpi=520)
-                    plt.show()
-                    plt.close()
-                #break
-
-            # resultFile = open("input/detection-results/result"+str(i)+".txt", "w")
-            # j = idx
-            # resultFile.write("%s %.6f %f %f %f %f\n"%(actions[int(label[j])], conf[j], bbox[j][0], 0, bbox[j][1], 90))
-            # resultFile.close()
-            # groundFile = open("input/ground-truth/result"+str(i)+".txt", "w")
-            # #for j in range(0, bboxV[idp].shape[0]):
-            # groundFile.write("%s %f %f %f %f\n"%(actions[int(labelV[idp][0])], bboxV[idp][0], 0, bboxV[idp][1], 90))
-            # groundFile.close()
-
             #print("预测:", bbox[idx].tolist()," ", str(conf[idx].tolist())[:5], " ",label[idx], " 真实:",bboxV[idp].tolist(), labelV[idp].tolist(), "iou=", max_iou)
+            
             if int(label[idx])==int(labelV[idp][0]):
                 acc = acc + 1
             ious_all += max_iou
             detection_all += dete_acc
+
             cnt+=1
 
 ious_all /= i
 detection_all /= i
 acc /= i
+
 _time /= i
 print("有效预测：",cnt)
 print("IOU: ",ious_all)
@@ -215,4 +176,4 @@ print("检测精度: ",detection_all)
 print("分类准确度：", acc)
 print("平均每份预测时间：", _time,"秒")
 
-os.system('python get_map.py')
+
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-Kaggle = True
+Kaggle = False
 
 
 
@@ -152,7 +152,7 @@ def fit_ont_epoch(net,epoch,epoch_size,epoch_size_val,gen,genval,Epoch,cuda,  be
     
 if __name__ == "__main__":
     # 设置训练的数据集
-    dataset_name = "TEMPORAL"
+    dataset_name = "192S1ALL"
     # 实验名
     log_name = "15-torch"
     
