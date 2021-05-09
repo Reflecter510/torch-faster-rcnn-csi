7146dff738bf6da3c262f401c89e33a0e69a2cac
reflecter510 <499300020@qq.com>
2021-05-09 11:20:25 +0800

    15-torch-linear384 temporal

--- train.py
+++ train.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
     dataset = TEMPORAL
    
     # 设置实验名
-    log_name = "15-torch"
+    log_name = "15-torch-linear384"
     
     # 设置主干特征提取网络类型
     BACKBONE = "alexnet"
