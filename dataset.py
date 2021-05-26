c9591dd40fc68bbc43736d695ad744a22da67b6c
reflecter510 <499300020@qq.com>
2021-05-26 13:20:17 +0800

    S0  S2

--- dataset.py
+++ dataset.py
@@ -20,3 +20,13 @@ class TEMPORAL():
     kaggle_dir = "../input/my"
     actions = ['none', "hand_up", "hand_down", "hand_left", "hand_right", "hand_circle", "hand_cross"]
 
+class S2():
+    name = "192S2"
+    image_shape = [90,192,1]
+    num_classes = 12
+    anchor =  ((4*16,5*16,6*16,7*16,8*16,9*16,10*16),)
+    train_batch = 288
+    test_batch = 72
+    kaggle_dir = "../input/mydata/S2"
+    actions = ['none', 'jump', 'pick', 'throw', 'pull', 'clap', 'box', 'wave', 'lift', 'kick', 'squat', 'turnRound', 'checkWatch']
+    
